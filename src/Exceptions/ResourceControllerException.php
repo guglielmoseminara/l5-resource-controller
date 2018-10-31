b633a053091777bed2178937544d5bb8e1661d89
Mario Patronelli <mario@raffles.com.ar>
2018-10-31 20:03:06 +0000

    Return valid Json Internal Server Error response on exception

--- src/Exceptions/ResourceControllerException.php
+++ src/Exceptions/ResourceControllerException.php
@@ -19,10 +19,34 @@ class ResourceControllerException extends \Exception
      *
      * @param \Illuminate\Http\Request $request The request object.
      *
-     * @return \Illuminate\Http\RedirectResponse
+     * @return mixed
      */
     public function render($request)
     {
-        return redirect()->back()->with(['rafflesargentina.status.error' => $this->message]);
+        if ($request->wantsJson()) {
+            return $this->validInternalServerErrorJsonResponse($this->message);
+        } else {
+            return redirect()->back()->with(['rafflesargentina.status.error' => $this->message]);
+        }
+    }
+
+    /**
+     * Return a valid 500 Internal Server Error json response.
+     *
+     * @param string $message The response message.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function validInternalServerErrorJsonResponse($message = 'Error')
+    {
+        return response()->json(
+            [
+                'exception' => class_basename($this),
+                'file' => basename($this->getFile()),
+                'line' => $this->getLine(),
+                'message' => $this->getMessage(),
+                'trace' => $this->getTrace(),
+            ], 500, [], JSON_PRETTY_PRINT
+        );
     }
 }
